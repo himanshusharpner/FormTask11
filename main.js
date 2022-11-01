576496d9ab394516455583472c5685923c0eb5dd
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-11-01 20:41:39 +0530

    detailshowondom and seealluserinlocalstorage

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-var btn = document.querySelector('.btn');
-btn.addEventListener('click', (e)=>{
-    document.querySelector('.btn').style.color = "white";
-    document.querySelector('.btn').style.background = "black";
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const emailId = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    if (emailId.length > 0 && name.length > 0) {
+      const object = {
+        name: name,
+        emailId: emailId 
+      };
+      localStorage.setItem(emailId, JSON.stringify(object));
+      addNewLineElement(object);
+    }
+  });
 
-});
-
-
-// storing the user details as an object in the local storage
-function saveToLocalStorage(event){
-    event.preventDefault();
-    const name = event.target.username.value;
-    const email = event.target.emailId.value;
-    let myobj = {
-        name ,
-        email,
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("Himanshu");
+    localStorage.getItem();
+  });
+  
+  if (document.readyState !== "loading") {
+  
+    var keys = Object.keys(localStorage)
+      i = keys.length; 
+    console.log("keys", keys);
+    let a, b;
+  
+    // 6 to 0
+    Object.keys(localStorage).forEach((key) => {
+      //i==2
+      if (key.match(/userDetails/g)) {
+        a = localStorage.getItem(key);
+        b = JSON.parse(a);
+  
+        addNewLineElement(b);
       }
-     let myObj_serilized = JSON.stringify(myobj);
+    });
+  }
 
-     localStorage.setItem('myobj',myObj_serilized);
-
-     let myobj_deserialized = JSON.parse(localStorage.getItem('obj'));
-
-     //console.log(myobj_deserialized);
-} 
+  function addNewLineElement(object) {
+    const ul = document.getElementById("listOfUser");
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(object.name + " " + object.emailId + " ")
+    );
+    ul.appendChild(li);
+  }
